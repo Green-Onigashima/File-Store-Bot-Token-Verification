de898ce90a34517d37921c84e080d49b7bb94d8c
Green-Onigashima <greenmorongetlostagain@gmail.com>
2024-07-28 12:26:38 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -9,7 +9,7 @@ from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats','auth_secret','deauth_secret', 'auth', 'sbatch', 'exit', 'bought', 'add_admin', 'del_admin', 'admins', 'add_prem', 'ping', 'restart']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Please Wait...! 🫷", quote = True)
+    reply_text = await message.reply_text("**Please have patience...! 🫷**", quote = True)
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except FloodWait as e:
@@ -17,16 +17,16 @@ async def channel_post(client: Client, message: Message):
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
-        await reply_text.edit_text("Something went Wrong..!")
+        await reply_text.edit_text("**Something went Wrong..!**")
         return
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Here is your Link", url=f'{link}')]])
 
-    await reply_text.edit(f"<b>Here is your link</b>🔗\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<blockquote><b>📯 Here is your Link\n\n{link}</b><blockquote>", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         try:
@@ -47,7 +47,7 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Here is your Link", url=f'{link}')]])
     try:
         await message.edit_reply_markup(reply_markup)
     except FloodWait as e:
