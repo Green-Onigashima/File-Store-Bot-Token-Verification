627c47ab9373ef3283377385bd1374d1cf0b3353
Green-Onigashima <greenmorongetlostagain@gmail.com>
2024-07-29 07:31:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 from aiohttp import web
 from database.database import full_adminbase
 from plugins import web_server
-from pyrogram import Client
-from pyrogram.enums import ParseMode
+from pyrogram import Client, enums
 import sys
 from pyromod import listen
 from datetime import datetime
+from config import *
 
-from config import ADMINS, API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL,FORCE_SUB_CHANNEL2, CHANNEL_ID, PORT, OWNER_ID
 
 class Bot(Client):
     def __init__(self):
@@ -23,44 +22,43 @@ class Bot(Client):
         )
         self.LOGGER = LOGGER
 
+            
     async def start(self):
-        await super().start()
-        usr_bot_me = await self.get_me()
-        self.uptime = datetime.now()
-
-        if FORCE_SUB_CHANNEL:
-            try:
-                link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
-                    link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
-                self.invitelink = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                sys.exit()
-        if FORCE_SUB_CHANNEL2:
-            try:
-                link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL2)
-                    link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
-                self.invitelink2 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
-                sys.exit()
+        try:
+            await super().start()
+            usr_bot_me = await self.get_me()
+            self.username = usr_bot_me.username
+            self.namebot = usr_bot_me.first_name
+            self.uptime = datetime.now()
+            self.LOGGER(__name__).info(
+                f"🟢 TG_BOT_TOKEN detected!\nFirst Name: {self.namebot}\nUsername: @{self.username}\n"
+            )
+        except Exception as a:
+            self.LOGGER(__name__).warning(a)
+            self.LOGGER(__name__).info("🔴 Bot Stopped.")
+            sys.exit()
+           
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            test = await self.send_message(chat_id=db_channel.id, text="Test Message", disable_notification=True)
             await test.delete()
+            self.LOGGER(__name__).info(
+                f"🟢 CHANNEL_ID Database detected!\nTitle: {db_channel.title}\n Chat ID: {db_channel.id}\n"
+            )
         except Exception as e:
             self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).warning(
+                f"🔴 Make sure @{self.username} is an admin in your DataBase channel, {CHANNEL_ID}"
+            )
+            self.LOGGER(__name__).info("🔴 Bot Stopped.")
             sys.exit()
+
+        self.set_parse_mode(enums.ParseMode.HTML)
+        self.LOGGER(__name__).info(f"""🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢""")
+        self.LOGGER(__name__).info("🥵 Bot is Running..!💨")
+        self.LOGGER(__name__).info("🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢")
+
         
         initadmin = await full_adminbase()
         for x in initadmin:
@@ -69,16 +67,10 @@ class Bot(Client):
             ADMINS.append(x)
         await self.send_message(
             chat_id=OWNER_ID,
-            text="Bot has started! 😉"
+            text="**Bot has started! 😉**"
         )
 
-
-        self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!")
-        self.username = usr_bot_me.username
-
         
-        #web-response
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
