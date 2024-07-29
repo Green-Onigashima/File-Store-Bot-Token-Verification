2321a8b8cfbf39270d1b719ccc4aa18e73240e89
Green-Onigashima <greenmorongetlostagain@gmail.com>
2024-07-29 07:07:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from operator import add
 import os
-from os import environ,getenv
+from os import environ, getenv
 import logging
 #import dotenv
 
@@ -9,111 +9,53 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #force user to join your backup channel leave 0 if you don't need.
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
+#FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+#FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
 
-if FORCE_SUB_CHANNEL > FORCE_SUB_CHANNEL2:
-    temp = FORCE_SUB_CHANNEL2 
-    FORCE_SUB_CHANNEL2 = FORCE_SUB_CHANNEL
-    FORCE_SUB_CHANNEL = temp
+#if FORCE_SUB_CHANNEL > FORCE_SUB_CHANNEL2:
+#    temp = FORCE_SUB_CHANNEL2 
+#    FORCE_SUB_CHANNEL2 = FORCE_SUB_CHANNEL
+#    FORCE_SUB_CHANNEL = temp
 
-#bot stats
-BOT_STATS_TEXT = os.environ.get("BOTS_STATS_TEXT","<b>BOT UPTIME 🌺</b>\n{uptime}")
-#send custom message when user interact with bot
-USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "ʙʀᴜʜ ᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ sᴇɴᴘᴀɪ!!")
+BOT_STATS_TEXT = os.environ.get("BOTS_STATS_TEXT","<blockquote><b>BOT UPTIME: {uptime}</b></blockquote>")
 
-#your bot token here from https://telegram.me/BotFather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "") 
-#your api id from https://my.telegram.org/apps
-APP_ID = int(os.environ.get("APP_ID", ""))
-#your api hash from https://my.telegram.org/apps
-API_HASH = os.environ.get("API_HASH", "")
-#your channel_id from https://t.me/MissRose_bot by forwarding dummy message to rose and applying command `/id` in reply to that message
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-#your database channel link
-CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "")
-#your id of telegram can be found by https://t.me/MissRose_bot with '/id' command
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-#port set to default 8080
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6105757517:AAHS2KgJDLuEErCoLyxkfOjCYwUhOZSvnlY") 
+APP_ID = int(os.environ.get("APP_ID", "25695562"))
+API_HASH = os.environ.get("API_HASH", "0b691c3e86603a7e34aae0b5927d725a")
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001732352061"))
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/+CT5g4kpXx-tlN2I1")
+OWNER_ID = int(os.environ.get("OWNER_ID", "1895952308"))
 PORT = os.environ.get("PORT", "8080")
-#your database url mongodb only You can use mongo atlas free cloud database
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://lavda:lassan@cluster0.9uhmlry.mongodb.net/")
-#your database name
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
-#for creating telegram thread for bot to improve performance of the bot
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-#your start default command message.
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
-#your telegram tag without @
-OWNER_TAG = os.environ.get("OWNER_TAG", "cosmic_freak")
-#Time in seconds for message delete
+START_MSG = os.environ.get("START_MESSAGE", "<blockquote><b>Hello {mention}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+OWNER_TAG = os.environ.get("OWNER_TAG", "StupidBoi69")
 TIME = int(os.environ.get("TIME", "60"))
 
+#USE_PAYMENT = True if (os.environ.get("USE_PAYMENT", "FALSE") == "TRUE") & (USE_SHORTLINK) else False
 
-# add premium logs channel id
-PAYMENT_LOGS = int(environ.get('PAYMENT_LOGS', '-1001868871195'))
-
-#Shortner (token system) 
-"""
-some token verification sites
-https://dashboard.shareus.io/
-"""
-
-# Turn this feature on or off using True or False put value inside  ""
-# TRUE for yes FALSE if no 
-USE_SHORTLINK = True if os.environ.get('USE_SHORTLINK', "FALSE") == "TRUE" else False 
-# only shareus service known rightnow rest you can test on your own
-SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "api.shareus.io")
-# SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "")
-#use this key if not working ☠️ (jokin!!)
-SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "")
-#add your custom time in secs for shortlink expiration.
-# 24hr = 86400
-# 12hr = 43200
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', "86400")) # Add time in seconds
-#Tutorial video for the user of your shortner on how to download.
-TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_Download_7x/32")
-
-#Payment to remove the token system
-#put TRUE if you want this feature
-USE_PAYMENT = True if (os.environ.get("USE_PAYMENT", "FALSE") == "TRUE") & (USE_SHORTLINK) else False
-#UPI ID
-UPI_ID = os.environ.get("UPI_ID", " ")
-#UPI QR CODE IMAGE
-UPI_IMAGE_URL = os.environ.get("UPI_IMAGE_URL", "")
-#SCREENSHOT URL of ADMIN for verification of payments
-SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", "")
-#Time and its price
-#7 Days
-PRICE1 = os.environ.get("PRICE1", "30 rs")
-#1 Month
-PRICE2 = os.environ.get("PRICE2", "110 rs")
-#3 Month
-PRICE3 = os.environ.get("PRICE3", "299 rs")
-#6 Month
-PRICE4 = os.environ.get("PRICE4", "550 rs")
-#1 Year
-PRICE5 = os.environ.get("PRICE5", "999 rs")
+PAYMENT_LOGS = int(environ.get('PAYMENT_LOGS', '-1001968254468'))
+USE_SHORTLINK = os.environ.get('USE_SHORTLINK', "True")
+SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "modijiurl.com")
+SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "a0c51b7b2b16924757c1e2eb6ca27096f9df7208")
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', "43200"))
+TUT_VID = os.environ.get("TUT_VID","https://t.me/Anime_Elixir/12")
+USE_PAYMENT = os.environ.get("USE_PAYMENT", "True")
+UPI_USERNAME = os.environ.get("UPI_USERNAME", "StupidBoi69")
 
 
 
-#force message for joining the channel
-FORCE_MSG = os.environ.get("FORCE_MSG", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b> 🥺")
-#custom caption 
+START_MSG = os.environ.get("START_MESSAGE", "<blockquote><b>ℹ️ Hello {mention}\nI can store private files in Specified Channel and other users can access it from special link. 💾</b></blockquote>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<blockquote><b>ℹ️ Hello {mention}\nYou need to join in my Channel to use me\nKindly Please join Channel</b></blockquote>")
+
+BOT_STATS_TEXT = "<b>BOT UPTIME {uptime}</b>"
+USER_REPLY_TEXT = "<blockquote><b>🔴 Don't send me messages directly I'm only File Share bot!\nTo resolve any issues contact bot developer: @StupidBoi69</b></blockquote>"
+
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-#protected content so that no files can be sent from the bot to anyone. recommended False
-# TRUE for yes FALSE if no
-PROTECT_CONTENT = True if os.environ.get("PROTECT_CONTENT", "FALSE") == "TRUE" else False
-#used if you dont need buttons on database channel.
-# True for yes False if no
-DISABLE_CHANNEL_BUTTON = True if os.environ.get("DISABLE_CHANNEL_BUTTON", "TRUE") == "TRUE" else False
-#you can add admin inside the bot
-
-
-
-
-#no need to add anything from now on
+PROTECT_CONTENT = os.environ.get("PROTECT_CONTENT", "False")
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", "True")
 
 ADMINS = []
 ADMINS.append(OWNER_ID)
